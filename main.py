94ed4cced6c3c07a3f1aadc14fbd2cac66db287b
Cherrysourballs <griever.lory@gmail.com>
2026-04-28 03:56:38 +0200

    B: added user prompting

--- main.py
+++ main.py
@@ -5,10 +5,23 @@ api_key = os.environ.get("GEMINI_API_KEY")
 if api_key == None:
     raise RuntimeError("No api_key found")
 
+import argparse
 from google import genai
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+# Now we can access `args.user_prompt`
 client = genai.Client(api_key=api_key)
-response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
+prompt_token_count = response.usage_metadata.prompt_token_count
+candidates_token_count = response.usage_metadata.candidates_token_count
+if response.usage_metadata != "None":
+    print(f"Prompt tokens: {prompt_token_count}\nResponse tokens: {candidates_token_count}")
+else:
+    raise RuntimeError("Failed API request, no usage_metadata")
 print(response.text)
+
+
 
 def main():
     print("Hello from aiagent!")
