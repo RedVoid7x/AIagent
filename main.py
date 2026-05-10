554d42cb052696ad891fdc55aef8c1aab1f71db6
Cherrysourballs <griever.lory@gmail.com>
2026-05-11 01:17:36 +0200

    I: made agent able to call functions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from prompts import system_prompt
-from call_functions import available_functions
+from call_functions import available_functions, call_function
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 if api_key == None:
@@ -29,8 +29,19 @@ def main():
     else:
         raise RuntimeError("Failed API request, no usage_metadata")
     if response.function_calls != None:
+        function_results = []
         for function_call in response.function_calls:
-            print(f"Calling function: {function_call.name}({function_call.args})")
+            function_call_result = call_function(function_call, args.verbose)
+            if not function_call_result.parts:
+                raise Exception("Invalid")
+            if function_call_result.parts[0].function_response is None:
+                raise Exception("Invalid")
+            if function_call_result.parts[0].function_response.response is None:
+                raise Exception("Invalid")
+            function_results.append(function_call_result.parts[0])
+            if args.verbose:
+                print(f" -> {function_call_result.parts[0].function_response.response}")
+
     else:
         print(response.text)
 
