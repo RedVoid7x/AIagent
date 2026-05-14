763c33eb5696a18a933823d575841b088a7f1923
Cherrysourballs <griever.lory@gmail.com>
2026-05-14 06:10:41 +0200

    K: finalized agent

--- test_get_files_info.py
+++ test_get_files_info.py
@@ -2,10 +2,10 @@ from functions.get_files_info import get_files_info
 print("Result for current directory:")
 print(get_files_info("calculator", "."))
 
-print("Result for pkg directory:")
+print("Result for 'pkg' directory:")
 print(get_files_info("calculator", "pkg"))
 
-print("Result for bin directory:")
+print("Result for '/bin' directory:")
 print(get_files_info("calculator", "/bin"))
 
 print("Result for '../' directory:")
